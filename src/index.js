0168049046a8311e23d4ee762b23209e2233cf31
grey4uk <57910084+grey4uk@users.noreply.github.com>
2022-11-25 13:29:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store, persistor } from 'redux/store';
 import App from 'components/App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import './styles/index.css';
 import Loader from 'components/Loader';
@@ -13,9 +13,9 @@ root.render(
   <React.StrictMode>
     <PersistGate loading={<Loader />} persistor={persistor}>
       <Provider store={store}>
-        <BrowserRouter basename="/checking-deploy-routing">
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
     </PersistGate>
   </React.StrictMode>
